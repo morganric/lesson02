e810c1a62475aaea85cd7658358b1aa2a9b8b7e1
morganric <rich@embedtree.com>
2012-09-26 19:11:14 +0100

    answers to variables lab

--- variables.rb
+++ variables.rb
@@ -1,9 +1,16 @@
 # 1. declare three variables and assign them odd-integer values in a single expression
 
+a,b,c = 3,5,7
+
 
 # 2. write an expression dividing two of the Task # 1 variables, but displaying the full fractional remainder
 # Hint: turn an integer into a float
 
+puts  a / b.to_f
+
 
 # 3. declare a name variable, assign it your name, add one of your integer variables to it, and display the result
 
+name = "rich"
+
+puts name + b.to_s
