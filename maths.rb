3805c5a7b27a8d6a8c1b23f41ac851bf06261aff
morganric <rich@embedtree.com>
2012-09-26 18:45:18 +0100

    answers for maths

--- maths.rb
+++ maths.rb
@@ -5,15 +5,29 @@ c = 5
  
 # 1. use ** and + in a single expression to display the result 21
 
+puts a**b + c
+
  
 # 2. use * and + in a single expression to display the result 13
+
+puts a*b + c
 
  
 # 3. using only parentheses, change expression #2 to display 28
 
+puts a* (b + c)
  
 # 4. display the result of 2 / 5 followed by 2.0 / 5
+
+puts b / c
+puts b.to_f / c
 
 
 # 5. write four expressions, two displaying even numbers modulus 2
 #    and two displaying odd numbers modulus 2. notice a pattern?
+
+
+puts a % 2
+puts b % 2
+puts c % 2
+puts (c + b) % 2
