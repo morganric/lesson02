3b028fa97a434a7706be163fc45dfb2569eea8ca
morganric <rich@embedtree.com>
2012-09-26 19:45:41 +0100

    answers to arrays

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,25 @@
 # 1. create an array with three different string values
 
+websites = ["Facebook", "Google", "Twitter"]
+
+puts websites
+
 # 2. add two new values to the Task #1 array in single expression
+
+websites.push("Wikipedia", "StackOverFlow")
+
+puts websites
 
 # 3. convert your name to an array, and display it, in a single expression
 
+fullname = ["Richard", "Morgan"]
+
+puts fullname
+
 # 4. remove and display the last value in the Task # 1 array, and discuss
 # with the instructor why both values added in Task # 2 may have been removed
+
+puts websites.pop
+
+
+# QUESTION? with the instructor why both values added in Task # 2 may have been removed
--- variables.rb
+++ variables.rb
@@ -6,7 +6,7 @@ a,b,c = 3,5,7
 # 2. write an expression dividing two of the Task # 1 variables, but displaying the full fractional remainder
 # Hint: turn an integer into a float
 
-puts  a / b.to_f
+puts  a.to_f / c
 
 
 # 3. declare a name variable, assign it your name, add one of your integer variables to it, and display the result
